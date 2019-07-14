e65a9209fcc9b8306e58e5ff5cec3bc933831856
InteractiveTimmy <interactivetimmy@gmail.com>
2019-07-14 17:57:09 -0400

    feat(scripts): create entry for scripts
    Create entry for scripts to enable access to all
    of the generated methods.

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+const functions = require('./functions/index');
+const utils = require('./utils/index');
+
+process.app = {
+  config: utils.confs(process.env.NODE_ENV || 'dev'),
+  log: utils.logs,
+  args: utils.args(process.argv),
+};
+
+const {
+  config,
+  log,
+  args,
+} = process.app;
+
+switch (args.command) {
+  case 'build': functions.build(args); break;
+  case 'test': log('command "test" is not yet built', 'WARN'); break;
+  case 'start': log('command "start" is not yet built', 'WARN'); break;
+  default: log(`unknown command "${args.command}"`, 'ERROR');
+}
