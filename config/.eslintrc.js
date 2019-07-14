43bc7334fb2212658dd196a8e2da362305e4e5d9
InteractiveTimmy <interactivetimmy@gmail.com>
2019-07-14 17:47:16 -0400

    chore(config): add linting to config scripts
    add linting to config scrips and their directories.

---
+++ config/.eslintrc.js
@@ -0,0 +1,24 @@
+const config = require('./index');
+
+const rules = {
+  ...config.linting.rules.base,
+};
+
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules,
+};
