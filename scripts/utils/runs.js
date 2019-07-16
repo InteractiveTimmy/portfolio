14ad53be560e4df5b115466d3569b7b4243559c8
InteractiveTimmy <interactivetimmy@gmail.com>
2019-07-15 20:54:41 -0400

    fix(scripts-utils): update runs script to spawn
    Update the runs utility to utilize spawn over
    exec, as exec is very limited to what kind of
    data it will output.

--- scripts/utils/runs.js
+++ scripts/utils/runs.js
@@ -1,13 +1,33 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
 module.exports = command => new Promise(
   (resolve, reject) => {
-    exec(command, (err, stdout) => {
+    const fc = command.split(' ');
+    if (process.platform === 'win32') {}
+    const script = (process.platform === 'win32') ? 'npm.cmd' : 'npm';
+
+    const cp = spawn(script, fc.splice(1), { stdio: 'inherit' })
+      .on('exit', (e) => {
+        if (e === 0) {
+          resolve(`finished ${script} ${fc.join(' ')}`);
+          cp.kill();
+        } else {
+          reject();
+        }
+       });
+    /*
+    const composer = exec(command, ['-c'], (err, stdout) => {
       if (err) {
-        reject(err);
+        reject(stdout);
       } else {
         resolve(stdout);
       }
     });
+
+    composer.stdout.on(
+      'data',
+      
+    )
+    */
   },
 );
